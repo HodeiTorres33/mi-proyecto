adf797e0d47d9f1b3aed0602b622aaa31050a68b
HodeiTorres33 <hodeitorres3@gmail.com>
2025-11-04 09:05:31 +0100

    Este commit es en la rama nueva.

---
+++ src/principal/nota.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class nota {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es el ejercicio 3.");
+	}
+
+}
