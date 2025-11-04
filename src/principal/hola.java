5f4b16e60b54de8800bbddd7c2459b783224934d
HodeiTorres33 <hodeitorres3@gmail.com>
2025-11-04 09:47:00 +0100

    Hotfix: corregido las pruebas

--- src/principal/hola.java
+++ src/principal/hola.java
@@ -5,6 +5,7 @@ public class hola {
 	public static void main(String[] args) {
 		
 		System.out.println("Parte 2 del ejercicio!!!!");
+		System.out.println("Pruebas");
 		
 	}
 	
--- src/principal/nota.java
+++ src/principal/nota.java
@@ -5,6 +5,7 @@ public class nota {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es el ejercicio 3.");
+		System.out.println("Prueba");
 	}
 
 }
